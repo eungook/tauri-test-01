1f0c71e85dcb6db248668dec78b2c271f593d43e
YI Eun Gook <yi.eungook@gmail.com>
2023-01-07 15:23:37 +0900

    prettier 미리 커밋

--- src/main.js
+++ src/main.js
@@ -5,13 +5,13 @@ let greetMsgEl;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  greetMsgEl.textContent = await invoke('greet', { name: greetInputEl.value });
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
+window.addEventListener('DOMContentLoaded', () => {
+  greetInputEl = document.querySelector('#greet-input');
+  greetMsgEl = document.querySelector('#greet-msg');
   document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
+    .querySelector('#greet-button')
+    .addEventListener('click', () => greet());
 });
